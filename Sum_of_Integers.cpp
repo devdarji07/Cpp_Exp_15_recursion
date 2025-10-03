54e42cd95ea304ef1cf1ccf8d9375e90346f7f15
Dev <devkdarji@gmail.com>
2025-10-03 22:11:19 +0530

    Create Sum_of_Integers.cpp

---
+++ Sum_of_Integers.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int sum(int n){
+    if(n<=1){ 
+        return 1;
+    }
+    else{
+        return (n+sum(n-1)); 
+    }
+}
+
+int main(){
+    
+    cout<<sum(8);
+    return 0;  
+}
+
+//OUTPUT:
+/*
+36
+*/
