9ee2b0ff5b8bfcd58f7f7e045395141e154467ff
Dev <devkdarji@gmail.com>
2025-10-03 22:11:47 +0530

    Create Printing_string_in_reverse.cpp

---
+++ Printing_string_in_reverse.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+void reverse(char*str){
+   if (*str) 
+   {
+       reverse(str+1); 
+       cout<<("%c", *str);
+   }
+}
+ 
+int main(){
+   char a[50];
+   cout<<"Enter a string: ";
+   cin>>a;
+   reverse(a); 
+   return 0;
+}
+
+//OUTPUT:
+/*
+Enter a string: Mishree
+eerhsiM
+*/
