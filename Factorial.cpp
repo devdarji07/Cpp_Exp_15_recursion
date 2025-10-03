7b7401c207606f61a5cf19f8d0d6628e19fb652f
Dev <devkdarji@gmail.com>
2025-10-03 22:12:25 +0530

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int factorial(int n){
+    if(n<=1){
+        return 1;
+    }
+    else{
+        return(n*factorial(n-1));
+    }
+}
+int main(){
+    int a;
+    cout<<"Enter number to find factorial: ";
+    cin>>a;
+    cout<<"Factorial of "<<a<<" is: "<<factorial(a);
+    
+    return 0;
+}
+
+//OUTPUT:
+
+//Enter number to find factorial: 4
+//Factorial of 4 is: 24
